a74faaebe7fd31ac491174a1ab1ff7a198419d8e
rch <rch@192.168.8.161>
2024-10-13 23:45:30 +0200

    reformat code

--- GETT/test.cpp
+++ GETT/test.cpp
@@ -1,9 +1,16 @@
 // test file
- class alfa {
- private:
-	 int i;
- public:
-	 alfa() {
-		 i=0;
-	 }
- };
+class alfa {
+private:
+	int i;
+public:
+	alfa() {
+		i = 0;
+	}
+	void setI(int k) {
+		i = k;
+	}
+
+	int getI() {
+		return i;
+	}
+};
